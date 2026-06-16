321d69d0e50de723687a3e03f5bcae0da27cdf04
lindarahma570 <yvzygr7n45@privaterelay.appleid.com>
2026-06-16 23:20:31 +0800

    Add files via upload

---
+++ Perulangan While.py
@@ -0,0 +1,13 @@
+#while loop
+ 
+#Kondiri
+#Aksi
+while True:
+   angka = int(input("Masukkan angka: "))
+   
+   if angka < 0:
+       print("Harus positif!")
+   else:
+       break
+ 
+print("Angka yang dimasukkan:", angka)
